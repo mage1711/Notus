064a1ffd8ed6adcc20b3a024274e56e86b7e52fe
ziad abdelrahman <zizo17111998@gmail.com>
2021-04-27 15:49:54 +0200

    added basic express api

---
+++ server/src/app.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const PORT = 5000;
+app.get('/', (req, res) => res.send('Express'));
+app.listen(PORT, () => {
+  console.log(`⚡[server]: Server is running at http://localhost:${PORT}`);
+});
